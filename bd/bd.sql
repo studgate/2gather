c8329c0f7db3de4e22dd3fd19ea1c596752148bb
joaoteixeira <joaoteixeira@aroundknowledge.com>
2015-02-05 11:10:24 +0000

    chg: dataframe: Changed some tables types.

--- bd/bd.sql
+++ bd/bd.sql
@@ -25,9 +25,9 @@ DROP TABLE IF EXISTS `tbl_job`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_job` (
-  `ID` int(11) NOT NULL,
-  `Title` varchar(50) DEFAULT NULL,
-  `Description` varchar(150) DEFAULT NULL,
+  `ID` bigint(20) NOT NULL,
+  `Title` varchar(70) DEFAULT NULL,
+  `Description` varchar(500) DEFAULT NULL,
   `Type` enum('Permanent','Part-Time','Full-Time') DEFAULT NULL,
   `Experience` varchar(45) DEFAULT NULL,
   `BeginDate` date DEFAULT NULL,
@@ -35,7 +35,10 @@ CREATE TABLE `tbl_job` (
   `TravelRequired` enum('TRUE','FALSE') DEFAULT NULL,
   `RelocationCovered` enum('TRUE','FALSE') DEFAULT NULL,
   `Posted_date` date DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+  `ID_recruiter` bigint(20) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_tbl_job_1_idx` (`ID_recruiter`),
+  CONSTRAINT `fk_tbl_job_1` FOREIGN KEY (`ID_recruiter`) REFERENCES `tbl_recruiter` (`ID`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -56,16 +59,16 @@ DROP TABLE IF EXISTS `tbl_job_location`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_job_location` (
-  `ID_job` int(11) NOT NULL,
-  `ID_location` int(11) NOT NULL,
-  `Street` varchar(60) DEFAULT NULL,
+  `ID_job` bigint(20) NOT NULL,
+  `ID_location` bigint(20) NOT NULL,
+  `Street` varchar(70) DEFAULT NULL,
   `PostalCode` varchar(20) DEFAULT NULL,
   `Latitude` float DEFAULT NULL,
   `Longitude` float DEFAULT NULL,
   PRIMARY KEY (`ID_job`,`ID_location`),
   KEY `fk_tbl_job_location_2_idx` (`ID_location`),
-  CONSTRAINT `fk_tbl_job_location_1` FOREIGN KEY (`ID_job`) REFERENCES `tbl_job` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tbl_job_location_2` FOREIGN KEY (`ID_location`) REFERENCES `tbl_locations` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_tbl_job_location_2` FOREIGN KEY (`ID_location`) REFERENCES `tbl_locations` (`ID`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `fk_tbl_job_location_1` FOREIGN KEY (`ID_job`) REFERENCES `tbl_job` (`ID`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -86,12 +89,12 @@ DROP TABLE IF EXISTS `tbl_job_payment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_job_payment` (
-  `ID_job` int(11) NOT NULL,
-  `ID_payment` int(11) NOT NULL,
+  `ID_job` bigint(20) NOT NULL,
+  `ID_payment` bigint(20) NOT NULL,
   PRIMARY KEY (`ID_job`,`ID_payment`),
   KEY `fk_tbl_job_payment_2_idx` (`ID_payment`),
-  CONSTRAINT `fk_tbl_job_payment_1` FOREIGN KEY (`ID_job`) REFERENCES `tbl_job` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tbl_job_payment_2` FOREIGN KEY (`ID_payment`) REFERENCES `tbl_payment` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_tbl_job_payment_1` FOREIGN KEY (`ID_job`) REFERENCES `tbl_job` (`ID`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `fk_tbl_job_payment_2` FOREIGN KEY (`ID_payment`) REFERENCES `tbl_payment` (`ID`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -105,32 +108,6 @@ LOCK TABLES `tbl_job_payment` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `tbl_job_recruiter`
---
-
-DROP TABLE IF EXISTS `tbl_job_recruiter`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tbl_job_recruiter` (
-  `ID_job` int(11) NOT NULL,
-  `ID_recruiter` int(11) NOT NULL,
-  PRIMARY KEY (`ID_job`,`ID_recruiter`),
-  KEY `fk_tbl_job_recruiter_2_idx` (`ID_recruiter`),
-  CONSTRAINT `fk_tbl_job_recruiter_1` FOREIGN KEY (`ID_job`) REFERENCES `tbl_job` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tbl_job_recruiter_2` FOREIGN KEY (`ID_recruiter`) REFERENCES `tbl_recruiter` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tbl_job_recruiter`
---
-
-LOCK TABLES `tbl_job_recruiter` WRITE;
-/*!40000 ALTER TABLE `tbl_job_recruiter` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tbl_job_recruiter` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `tbl_job_req`
 --
 
@@ -138,12 +115,12 @@ DROP TABLE IF EXISTS `tbl_job_req`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_job_req` (
-  `ID_job` int(11) NOT NULL,
-  `ID_req` int(11) NOT NULL,
+  `ID_job` bigint(20) NOT NULL,
+  `ID_req` bigint(20) NOT NULL,
   PRIMARY KEY (`ID_job`,`ID_req`),
   KEY `fk_tbl_job_req_2_idx` (`ID_req`),
-  CONSTRAINT `fk_tbl_job_req_1` FOREIGN KEY (`ID_job`) REFERENCES `tbl_job` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tbl_job_req_2` FOREIGN KEY (`ID_req`) REFERENCES `tbl_requirements` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_tbl_job_req_1` FOREIGN KEY (`ID_job`) REFERENCES `tbl_job` (`ID`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `fk_tbl_job_req_2` FOREIGN KEY (`ID_req`) REFERENCES `tbl_requirements` (`ID`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -164,12 +141,12 @@ DROP TABLE IF EXISTS `tbl_job_skills`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_job_skills` (
-  `ID_job` int(11) NOT NULL,
-  `ID_skils` int(11) NOT NULL,
+  `ID_job` bigint(20) NOT NULL,
+  `ID_skils` bigint(20) NOT NULL,
   PRIMARY KEY (`ID_job`,`ID_skils`),
   KEY `fk_tbl_job_skills_2_idx` (`ID_skils`),
-  CONSTRAINT `fk_tbl_job_skills_1` FOREIGN KEY (`ID_job`) REFERENCES `tbl_job` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tbl_job_skills_2` FOREIGN KEY (`ID_skils`) REFERENCES `tbl_skills` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_tbl_job_skills_1` FOREIGN KEY (`ID_job`) REFERENCES `tbl_job` (`ID`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `fk_tbl_job_skills_2` FOREIGN KEY (`ID_skils`) REFERENCES `tbl_skills` (`ID`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -190,7 +167,7 @@ DROP TABLE IF EXISTS `tbl_locations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_locations` (
-  `ID` int(11) NOT NULL,
+  `ID` bigint(20) NOT NULL,
   `City` varchar(50) NOT NULL,
   `State` varchar(50) NOT NULL,
   `Country` varchar(40) NOT NULL,
@@ -215,9 +192,9 @@ DROP TABLE IF EXISTS `tbl_payment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_payment` (
-  `ID` int(11) NOT NULL,
-  `Min_Amount` int(11) DEFAULT NULL,
-  `Max_Amount` int(11) DEFAULT NULL,
+  `ID` bigint(20) NOT NULL,
+  `Min_Amount` int(8) DEFAULT NULL,
+  `Max_Amount` int(8) DEFAULT NULL,
   `Type` enum('Hour','Day','Week','Month','Year') DEFAULT NULL,
   `Exchange` varchar(5) DEFAULT NULL,
   PRIMARY KEY (`ID`)
@@ -241,10 +218,10 @@ DROP TABLE IF EXISTS `tbl_recruiter`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_recruiter` (
-  `ID` int(11) NOT NULL,
+  `ID` bigint(20) NOT NULL,
   `Name` varchar(70) DEFAULT NULL,
-  `PageUrl` varchar(150) DEFAULT NULL,
-  `LogoUrl` varchar(150) DEFAULT NULL,
+  `PageUrl` varchar(2083) DEFAULT NULL,
+  `LogoUrl` varchar(2083) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -266,8 +243,8 @@ DROP TABLE IF EXISTS `tbl_requirements`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_requirements` (
-  `ID` int(11) NOT NULL,
-  `Description` varchar(50) DEFAULT NULL,
+  `ID` bigint(20) NOT NULL,
+  `Description` varchar(70) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -289,8 +266,8 @@ DROP TABLE IF EXISTS `tbl_skills`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_skills` (
-  `ID` int(11) NOT NULL,
-  `Description` varchar(50) DEFAULT NULL,
+  `ID` bigint(20) NOT NULL,
+  `Description` varchar(70) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -313,4 +290,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-02-05  9:55:51
+-- Dump completed on 2015-02-05 11:09:34
